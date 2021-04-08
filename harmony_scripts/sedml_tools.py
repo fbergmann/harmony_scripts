32c2e66aa172b14c537f5744cf05ece085ab7dac
Frank T. Bergmann <fbergman@caltech.edu>
2021-04-08 17:22:25 +0200

    - use libsedml 2.0.18 to get all ids / xpath expressions

--- harmony_scripts/sedml_tools.py
+++ harmony_scripts/sedml_tools.py
@@ -60,3 +60,97 @@ def check_files_exist(sedml_file_name):
             result = False
 
     return result
+
+
+def _check_for_duplicated_ids_in_list(sed_list, all_ids):
+    # type: (libsedml.SedListOf) -> bool
+
+    result = True
+
+    for element in sed_list:
+        id = element.id
+
+        if id not in all_ids:
+            all_ids[id] = 0
+        else:
+            result = False
+
+        all_ids[id] = all_ids[id] + 1
+
+    return result
+
+
+class _IdFilter(libsedml.SedElementFilter):
+
+    def __init__(self):
+        libsedml.SedElementFilter.__init__(self)
+        self.ids = []
+        self.ids_per_type = {}
+        self.messages = []
+        self.duplicated_ids = []
+
+    def filter(self, element):
+        assert(isinstance(element, libsedml.SedBase))
+        if element.isSetId():
+            tc = element.getTypeCode()
+            type_string = libsedml.SedTypeCode_toString(tc)
+            sed_id = element.getId()
+            if type_string not in self.ids_per_type:
+                self.ids_per_type[type_string] = []
+            if sed_id in self.ids:
+                self.duplicated_ids.append(sed_id)
+                self.messages.append(f'Duplicated id "{sed_id}" of type "{type_string}"')
+
+            self.ids.append(sed_id)
+            self.ids_per_type[type_string].append(sed_id)
+
+        return False
+
+
+def check_for_duplicated_ids(doc, print_warnings=False):
+    # type: (libsedml.SedDocument) -> bool
+
+    filter = _IdFilter()
+
+    elements = doc.getListOfAllElements(filter)
+
+    result = len(filter.messages) == 0
+
+    if print_warnings:
+        for message in filter.messages:
+            logging.warning(message)
+
+    return result
+
+
+class _XpathFilter(libsedml.SedElementFilter):
+
+    def __init__(self):
+        libsedml.SedElementFilter.__init__(self)
+        self.xpaths = []
+
+    def filter(self, element):
+        assert(isinstance(element, libsedml.SedBase))
+        tc = element.getTypeCode()
+
+        if isinstance(element, libsedml.SedVariable) or isinstance(element, libsedml.SedChange):
+            if element.isSetTarget():
+                target = element.getTarget()
+                if target not in self.xpaths:
+                    self.xpaths.append(target)
+        return False
+
+
+def get_xpath_expressions_from(doc):
+    # type: (libsedml.SedDocument) -> [str]
+
+    filter = _XpathFilter()
+
+    elements = doc.getListOfAllElements(filter)
+
+    return filter.xpaths
+
+
+if __name__ == "__main__":
+    pass
+
