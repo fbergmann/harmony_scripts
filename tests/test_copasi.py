b0e559cc346a8eb1b80bab08bc14fe9d5065bc19
Frank T. Bergmann <fbergman@caltech.edu>
2021-04-15 12:00:22 +0200

    - fix test

--- tests/test_copasi.py
+++ tests/test_copasi.py
@@ -21,9 +21,15 @@ class TestCOPASI(unittest.TestCase):
     def test_export_sedml(self):
         assert (isinstance(self.dm, COPASI.CDataModel))
         sedml = self.dm.exportSEDMLToString(None, 1, 2)
-        print(sedml)
-        self.assertTrue(self.dm.exportCombineArchive('../out/test_omex.omex', includeCOPASI=True,
-                                             includeSEDML=True, includeSBML=True))
+        if os.path.exists('./out/test_omex.omex'):
+            os.remove('./out/test_omex.omex')
+        self.dm.exportCombineArchive('./out/test_omex.omex',
+                                     includeCOPASI=True,
+                                     includeSEDML=True,
+                                     includeSBML=True,
+                                     includeData=True)
+
+        self.assertTrue(COPASI.CCopasiMessage.getHighestSeverity() <= COPASI.CCopasiMessage.WARNING)
 
 
 if __name__ == "__main__":
